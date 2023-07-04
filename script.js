0f007c7af44d787a171b2d5c566390d340d8f5d8
danielmjob <danielmarquesjob@gmail.com>
2023-07-04 12:12:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const evento2 = document.getElementById('enviar')
+
+function enviarFormularioWhatsapp(){
+
+        let nome = document.getElementById('nome').value;
+        let assunto = document.getElementById('assunto').value;
+        let mensagem = document.getElementById('mensagem').value;
+        let numero= 5561992371633;
+
+        
+
+        if (nome == "" & assunto=="" & mensagem==""){
+                console.log(" Campos obrigatórios Vazios");
+        }else{
+                var win= window.open(`https://wa.me/${numero}?text=*Nome:*%20${nome}%0*AAssunto:*%20${assunto}%0A*Mensagem:*%0A${mensagem}`,'_blank');
+
+                // %0A quebra a linha
+        }
+}
