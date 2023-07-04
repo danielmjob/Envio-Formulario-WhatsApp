129a7560c52d0d562dd912e84b34a11c21b3e007
danielmjob <danielmarquesjob@gmail.com>
2023-07-04 17:58:18 -0300

    adicionando comentários

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ function enviarFormularioWhatsapp(){
         let nome = document.getElementById('nome').value;
         let assunto = document.getElementById('assunto').value;
         let mensagem = document.getElementById('mensagem').value;
-        let numero= 5561992371633;
+        let numero= 5566000000000; 
 
+        /*
+        Número que irá receber a mensagem exemplo 5561000000000*
+        55 - Código do país
+        66 - DDD
+        000000000 - Número de telefone
+        */ 
         
-
-        if (nome == "" & assunto=="" & mensagem==""){
+        if (nome == "" | assunto=="" | mensagem==""){
                 console.log(" Campos obrigatórios Vazios");
         }else{
                 var win= window.open(`https://wa.me/${numero}?text=*Nome:*%20${nome}%0A*Assunto:*%20${assunto}%0A*Mensagem:*%0A${mensagem}`,'_blank');
